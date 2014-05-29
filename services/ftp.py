e9855039b1a67fe4c09ddacdf5fcb4698c1351eb
David Gay <oddshocks@riseup.net>
2014-05-29 12:40:11 -0400

    Skeleton starter service files.

---
+++ services/ftp.py
@@ -0,0 +1,8 @@
+#!/bin/env python
+# -*- coding: utf8 -*-
+
+
+def upload(image):
+    """ Takes an image location provided by Koji and uploads it
+        to the internal Fedora FTP server."""
+    pass
