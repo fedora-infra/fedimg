34667b59161f0079715e9bffa11237a1a8c5500f
David Gay <oddshocks@riseup.net>
2014-05-30 16:32:24 -0400

    Add some koji API code for testing getting build results/output.

--- fedimg/uploader.py
+++ fedimg/uploader.py
@@ -3,14 +3,28 @@
 
 import koji
 
+from pprint import pprint
+
 
 def upload(builds):
     """ Takes a list of one or more Koji build IDs (passed to it from
     consumer.py) and sends the appropriate image files off to cloud
     services. """
     if isinstance(builds, list):
-        for build in builds:
-            pass
+        # Create a Koji connection to the Fedora Koji instance
+        koji_server = "https://koji.fedoraproject.org/kojihub"
+        koji_session = koji.ClientSession(koji_server)
+        if len(builds) == 1:
+            print "\nRESULTS FOR listTaskOutput():\n"
+            pprint(koji_session.listTaskOutput())
+            print "\nRESULTS FOR getTaskResult():\n"
+            pprint(koji_session.getTaskResult())
+        elif len(builds) >= 2:
+            #koji.multicall = True
+            for build in builds:
+                print "\nPLACEHOLDER FOR MULTICALL BUILD SITUATION\n"
+            #results = koji.multiCall()
+            #koji.multicall = False  # TODO: Is this needed?
     else:
         # TODO: Not sure if this is the proper way to handle this.
         raise Exception("Build upload function must take a list.")
