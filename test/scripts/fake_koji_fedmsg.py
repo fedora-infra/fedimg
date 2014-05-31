078fc1e0a41e0081bee9e3a659836b9d629bec6d
David Gay <oddshocks@riseup.net>
2014-05-31 18:54:00 -0400

    PEP 8 the fake fedmsg script.

--- test/scripts/fake_koji_fedmsg.py
+++ test/scripts/fake_koji_fedmsg.py
@@ -13,7 +13,9 @@ fedmsg.init(active=True, name="relay_inbound")
 
 idx = '2014-707e188d-fc5e-4c9a-a4a2-9499beaafffe'
 
-resp = requests.get('https://apps.fedoraproject.org/datagrepper/id?id=%s' % idx)
+datagrepper_url = 'https://apps.fedoraproject.org/datagrepper/id?id=%s' % idx
+
+resp = requests.get(datagrepper_url)
 msg = resp.json()
 
 tokens = msg['topic'].split('.')
