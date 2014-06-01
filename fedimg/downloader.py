c201c6244315ed14ecf145f9e86aa239c5f10636
David Gay <oddshocks@riseup.net>
2014-06-01 14:26:34 -0400

    Put local file writing in try/except block to catch OSErrors.

--- fedimg/downloader.py
+++ fedimg/downloader.py
@@ -24,22 +24,26 @@ def download(urls):
         file_name = url.split('/')[-1]
         local_file_name = LOCAL_DOWNLOAD_DIR + file_name
         u = urllib2.urlopen(url)
-        with open(local_file_name, 'wb') as f:
-            meta = u.info()
-            file_size = int(meta.getheaders("Content-Length")[0])
+        try:
+            with open(local_file_name, 'wb') as f:
+                meta = u.info()
+                file_size = int(meta.getheaders("Content-Length")[0])
 
-            print "Downloading {0} ({1} bytes)".format(url, file_size)
-            bytes_downloaded = 0
-            block_size = 8192
-            while True:
-                buff = u.read(block_size)  # buffer
-                if not buff:
-                    break
+                print "Downloading {0} ({1} bytes)".format(url, file_size)
+                bytes_downloaded = 0
+                block_size = 8192
+                while True:
+                    buff = u.read(block_size)  # buffer
+                    if not buff:
+                        break
 
-                bytes_downloaded += len(buff)
-                f.write(buff)
-                bytes_remaining = float(bytes_downloaded) / file_size
-                status = r"{0}  [{1:.2%}]".format(bytes_downloaded,
-                                                  bytes_remaining)
-                status = status + chr(8) * (len(status) + 1)
-                sys.stdout.write(status)
+                    bytes_downloaded += len(buff)
+                    f.write(buff)
+                    bytes_remaining = float(bytes_downloaded) / file_size
+                    status = r"{0}  [{1:.2%}]".format(bytes_downloaded,
+                                                      bytes_remaining)
+                    status = status + chr(8) * (len(status) + 1)
+                    sys.stdout.write(status)
+        except OSError:
+            print "Problem writing to {}.".format(LOCAL_DOWNLOAD_DIR)
+            print "Make sure to run this service with root permissions."
