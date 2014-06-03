a66bf3e691e79e26ac02f0a641a5072791a07222
David Gay <oddshocks@riseup.net>
2014-06-03 11:42:24 -0400

    Add region list to ec2.py

--- fedimg/services/ec2.py
+++ fedimg/services/ec2.py
@@ -1,8 +1,23 @@
 #!/bin/env python
 # -*- coding: utf8 -*-
 
+import fedimg
+
+EC2_REGIONS = [
+    'EC2_AP_NORTHEAST',     # ap-northeast-1
+    'EC2_AP_SOUTHEAST',     # ap-southeast-1
+    'EC2_AP_SOUTHEAST2',    # ap-southeast-2
+    'EC2_EU_WEST',          # eu-west-1
+    'EC2_SA_EAST',          # sa-east-1
+    'EC2_US_EAST',          # (no special ID listed)
+    'EC2_US_WEST',          # us-west-1
+    'EC2_US_WEST_OREGON',   # us-west-2
+]
+
 
 def upload(image):
     """ Takes an image location provided by Koji and uploads it
         to all EC2 regions."""
-    pass
+
+    AMI_ID = 'ami-something123'
+    SIZE_ID = 'tx.something'
