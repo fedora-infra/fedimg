911e053e68b00dc6b8ecc230eb741d9a36f61816
David Gay <oddshocks@riseup.net>
2014-06-03 10:02:47 -0400

    Add a header comment to ftp.py

--- fedimg/services/ftp.py
+++ fedimg/services/ftp.py
@@ -1,6 +1,13 @@
 #!/bin/env python
 # -*- coding: utf8 -*-
 
+"""
+This file might contain some code later, but since this service will likely
+be running on a machine that can write files to the NFS share behind our FTP
+server, the local download code in downloader.py is sufficient, at least for
+now.
+"""
+
 
 def upload(image):
     """ Takes an image location provided by Koji and uploads it
