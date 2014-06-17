a8083b0c9687eb60c52e81bcf326c5c7f4d84196
David Gay <oddshocks@riseup.net>
2014-06-17 17:00:56 -0400

    Make message contain image URL and name, and resolve the merge conflict.

--- fedimg/messenger.py
+++ fedimg/messenger.py
@@ -9,14 +9,16 @@ https://github.com/fedora-infra/fedmsg_meta_fedora_infrastructure/blob/develop/f
 """
 
 
-def message(image_name, dest, status):
+def message(image_url, dest, status):
     """ Takes an image name, an upload destination (ex. "EC2-eu-west-1"), and a
     status (ex.  "failed"). Emits a fedmsg appropriate for each image upload
     task. """
 
     topic = 'image.upload'
+    image_name = image_url.split('/')[-1].replace('.raw.xz', '')
 
     fedmsg.publish(topic=topic, modname='fedimg', msg={
+        'image_url': image_url,
         'image_name': image_name,
         'destination': dest,
         'status': status,
