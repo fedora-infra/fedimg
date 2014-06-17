e751a19bd6d7f3ccedddbd7b64e200e1699bb37d
David Gay <oddshocks@riseup.net>
2014-06-17 13:53:11 -0400

    Break the topic out so that it can be tweaked later in the case more topics are added. For now, I don't see the need for any.

--- fedimg/messenger.py
+++ fedimg/messenger.py
@@ -8,7 +8,9 @@ def message(image_name, dest, status):
     """ Takes an upload destination (ex. "EC2-east") and a status (ex.
     "failed"). Emits a fedmsg appropriate for each image upload task. """
 
-    fedmsg.publish(topic='image.upload', modname='fedimg', msg={
+    topic = 'image.upload'
+
+    fedmsg.publish(topic=topic, modname='fedimg', msg={
         'image_name': image_name,
         'destination': dest,
         'status': status,
