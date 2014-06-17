b9a51283d9658c514ba0c6ea88305b16c6321efd
David Gay <oddshocks@riseup.net>
2014-06-17 14:13:50 -0400

    Improve commenting.

--- fedimg/messenger.py
+++ fedimg/messenger.py
@@ -3,10 +3,16 @@
 
 import fedmsg
 
+"""
+The latest Fedmsg meta code for Fedimg fedmsgs (what a mouthful!):
+https://github.com/fedora-infra/fedmsg_meta_fedora_infrastructure/blob/develop/fedmsg_meta_fedora_infrastructure/fedimg.py
+"""
+
 
 def message(image_name, dest, status):
-    """ Takes an upload destination (ex. "EC2-east") and a status (ex.
-    "failed"). Emits a fedmsg appropriate for each image upload task. """
+    """ Takes an image name, an upload destination (ex. "EC2-eu-west-1"), and a
+    status (ex.  "failed"). Emits a fedmsg appropriate for each image upload
+    task. """
 
     topic = 'image.upload'
 
