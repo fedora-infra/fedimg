074624a0d12752f01397c197d3b08ebd7b294484
David Gay <oddshocks@riseup.net>
2014-06-29 18:53:12 -0400

    Deploy a test node with a test script. Also move some stuff around.

--- fedimg/services/ec2.py
+++ fedimg/services/ec2.py
@@ -64,50 +64,52 @@ class EC2Service(object):
         node = None
         volume = None
         snapshot = None
-
-        ami = self.amis[0]  # DEBUG (us east x86_64)
-        cls = get_driver(ami['prov'])
-        driver = cls(fedimg.AWS_ACCESS_ID, fedimg.AWS_SECRET_KEY)
-
-        # select the desired node attributes
-        sizes = driver.list_sizes()
-        size_id = 'm1.large'
-        # check to make sure we have access to that size node
-        size = [s for s in sizes if s.id == size_id][0]
-        image = NodeImage(id=ami['ami'], name=None, driver=driver)
-
-        # deploy node
-        name = 'fedimg AMI builder'  # TODO: will add raw image title
-        # TODO: Make automatically-created /dev/sda be deleted on termination
-        mappings = [{'VirtualName': None,  # cannot specify with Ebs
-                     'Ebs': {'VolumeSize': 12,  # 12 GB should be enough
-                             'VolumeType': 'standard',
-                             'DeleteOnTermination': 'false'},
-                     'DeviceName': '/dev/sdb'}]
-
-        # read in ssh key
-        with open(fedimg.AWS_PUBKEYPATH, 'rb') as f:
-            key_content = f.read()
-
-        # Add key to authorized keys for root user
-        step_1 = SSHKeyDeployment(key_content)
-
-        # Add script for deployment
-        # Device becomes /dev/xvdb on instance due to recent kernel change
-        script = "sudo curl {0} | xzcat > /dev/xvdb".format(raw_url)
-        step_2 = ScriptDeployment(script)
-
-        # Create deployment object
-        msd = MultiStepDeployment([step_1, step_2])
-
-        # Fedmsg info
-        file_name = raw_url.split('/')[-1]
-        build_name = file_name.replace('.raw.xz', '')
-        destination = 'EC2 ({region})'.format(region=ami['region'])
-
+        test_node = None
+        
         fedimg.messenger.message(build_name, destination,
                                  'started')
+
         try:
+            ami = self.amis[0]  # DEBUG (us east x86_64)
+            cls = get_driver(ami['prov'])
+            driver = cls(fedimg.AWS_ACCESS_ID, fedimg.AWS_SECRET_KEY)
+
+            # select the desired node attributes
+            sizes = driver.list_sizes()
+            size_id = 'm1.large'
+            # check to make sure we have access to that size node
+            size = [s for s in sizes if s.id == size_id][0]
+            image = NodeImage(id=ami['ami'], name=None, driver=driver)
+
+            # deploy node
+            name = 'fedimg AMI builder'  # TODO: will add raw image title
+            # TODO: Make automatically-created /dev/sda be deleted on termination
+            mappings = [{'VirtualName': None,  # cannot specify with Ebs
+                         'Ebs': {'VolumeSize': 12,  # 12 GB should be enough
+                                 'VolumeType': 'standard',
+                                 'DeleteOnTermination': 'false'},
+                         'DeviceName': '/dev/sdb'}]
+
+            # read in ssh key
+            with open(fedimg.AWS_PUBKEYPATH, 'rb') as f:
+                key_content = f.read()
+
+            # Add key to authorized keys for root user
+            step_1 = SSHKeyDeployment(key_content)
+
+            # Add script for deployment
+            # Device becomes /dev/xvdb on instance due to recent kernel change
+            script = "sudo curl {0} | xzcat > /dev/xvdb".format(raw_url)
+            step_2 = ScriptDeployment(script)
+
+            # Create deployment object
+            msd = MultiStepDeployment([step_1, step_2])
+
+            # Fedmsg info
+            file_name = raw_url.split('/')[-1]
+            build_name = file_name.replace('.raw.xz', '')
+            destination = 'EC2 ({region})'.format(region=ami['region'])
+
             # Must be EBS-backed for AMI registration to work.
             node = driver.deploy_node(name=name, image=image, size=size,
                                       ssh_username='fedora',
@@ -142,15 +144,33 @@ class EC2Service(object):
 
             # Actually register image
             # TODO: Perhaps generate a description?
-            driver.ex_register_image(build_name,
-                                     description=None,
-                                     root_device_name='/dev/sda',
-                                     block_device_mapping=mapping,
-                                     architecture=get_file_arch(file_name))
+            arch = get_file_arch(file_name)
+            image = driver.ex_register_image(build_name,
+                                             description=None,
+                                             root_device_name='/dev/sda',
+                                             block_device_mapping=mapping,
+                                             architecture=arch))
 
-            # Emit success fedmsg
-            fedimg.messenger.message(build_name, destination,
-                                     'completed')
+            # Spin up a node of the AMI to test
+            # TODO: Need to report back status of tests for this to be useful
+
+            # Add script for deployment
+            # Device becomes /dev/xvdb on instance due to recent kernel change
+            script = fedimg.AWS_TEST
+            step_2 = ScriptDeployment(script)
+
+            # Create deployment object
+            msd = MultiStepDeployment([step_1, step_2])
+
+            test_node = driver.deploy_node(name=name, image=image, size=size,
+                                      ssh_username='fedora',
+                                      ssh_alternate_usernames=['root'],
+                                      ssh_key=fedimg.AWS_KEYPATH,
+                                      deploy=msd,
+                                      ex_keyname=fedimg.AWS_KEYNAME,
+                                      ex_security_groups=['ssh'],
+                                      ex_ebs_optimized=True)
+
 
         except DeploymentException as e:
             fedimg.messenger.message(build_name, destination,
@@ -171,3 +191,9 @@ class EC2Service(object):
                 driver.destroy_volume(volume)
             if snapshot:
                 driver.destroy_volume_snapshot(snapshot)
+            if test_node:
+                driver.destroy_node(test_node)
+
+        # Emit success fedmsg
+        fedimg.messenger.message(build_name, destination,
+                                 'completed')
