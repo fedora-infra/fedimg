6b849c49e701ce9636b5cc5e315b0e900f793564
David Gay <oddshocks@riseup.net>
2014-06-29 16:24:40 -0400

    Update test_upload.py script with up-to-date URL

--- test/scripts/test_upload.py
+++ test/scripts/test_upload.py
@@ -8,4 +8,4 @@ from fedimg.services.ec2 import EC2Service, EC2ServiceException
 
 ec2 = EC2Service()
 
-ec2.upload('https://kojipkgs.fedoraproject.org//work/tasks/5144/6925144/fedora-cloud-base-rawhide-20140604.x86_64.raw.xz')
+ec2.upload('https://kojipkgs.fedoraproject.org//work/tasks/9442/7049442/fedora-cloud-base-20140616-rawhide.x86_64.raw.xz')
