6276ae49ff1d0d06fdbb69c38660bf9425e9e899
David Gay <oddshocks@riseup.net>
2014-07-10 19:10:51 -0400

    Update test script (old build expired)

--- test/scripts/test_upload.py
+++ test/scripts/test_upload.py
@@ -8,4 +8,4 @@ from fedimg.services.ec2 import EC2Service, EC2ServiceException
 
 ec2 = EC2Service()
 
-ec2.upload('https://kojipkgs.fedoraproject.org//work/tasks/9442/7049442/fedora-cloud-base-20140616-rawhide.x86_64.raw.xz')
+ec2.upload('https://kojipkgs.fedoraproject.org//work/tasks/3868/7123868/fedora-cloud-base-20140710-rawhide.x86_64.raw.xz')
