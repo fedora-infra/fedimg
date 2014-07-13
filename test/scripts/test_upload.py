1a3a5b8fa84c96e54fd8339b96d2a3ec25aaf9dd
David Gay <oddshocks@riseup.net>
2014-07-13 13:47:54 -0400

    I've been told this is probably the best image to use for testing this stuff since it has the hd0 pvgrub fix

--- test/scripts/test_upload.py
+++ test/scripts/test_upload.py
@@ -8,4 +8,4 @@ from fedimg.services.ec2 import EC2Service, EC2ServiceException
 
 ec2 = EC2Service()
 
-ec2.upload('https://kojipkgs.fedoraproject.org//work/tasks/3868/7123868/fedora-cloud-base-20140710-rawhide.x86_64.raw.xz')
+ec2.upload('https://kojipkgs.fedoraproject.org//work/tasks/7840/7127840/fedora-cloud-base-20140711-rawhide.x86_64.raw.xz')
