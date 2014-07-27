c00981e26eb67d99b6409782df1f85f753e68934
David Gay <oddshocks@riseup.net>
2014-07-26 23:36:01 -0400

    Bump setup.py version to 0.2.0 for initial RPM release to Fedora

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='fedimg',
-    version='0.1.0',
+    version='0.2.0',
     description='A service that listens to the Fedmsg bus and'
                 ' automatically uploads built Fedora cloud images'
                 ' to internal and external cloud providers.',
