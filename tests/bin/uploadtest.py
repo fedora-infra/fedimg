02006c3d06e9ce1811225c846d35076479f90179
Ralph Bean <rbean@redhat.com>
2015-01-14 16:35:34 -0500

    Try initializing stdlib logging with fedmsg\'s logging config.

--- tests/bin/uploadtest.py
+++ tests/bin/uploadtest.py
@@ -5,6 +5,7 @@
     consume process. For development testing, mostly."""
 
 import fedmsg
+import fedmsg.config
 import fedimg
 import fedimg.services
 from fedimg.services.ec2 import EC2Service, EC2ServiceException
@@ -12,9 +13,10 @@ import fedimg.uploader
 from fedimg.util import virt_types_from_url
 
 import logging
-logging.basicConfig()
+import logging.config
+
+logging.config.dictConfig(fedmsg.config.load_config()['logging'])
 log = logging.getLogger('fedmsg')
-log.setLevel(logging.DEBUG)
 
 url = 'http://download.fedoraproject.org/pub/fedora/linux/releases/21/Cloud/Images/x86_64/Fedora-Cloud-Base-20141203-21.x86_64.raw.xz'
 
