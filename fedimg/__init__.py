c794d79fe51209d52ae7cff7c5cee2db317bc46b
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2017-07-08 22:13:14 +0530

    config: Migrate to mange the configuration using toml
    Signed-off-by: Sayan Chowdhury <sayan.chowdhury2012@gmail.com>

--- fedimg/__init__.py
+++ fedimg/__init__.py
@@ -1,5 +1,5 @@
 # This file is part of fedimg.
-# Copyright (C) 2014 Red Hat, Inc.
+# Copyright (C) 2014-2017 Red Hat, Inc.
 #
 # fedimg is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -17,45 +17,27 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 #
 # Authors:  David Gay <dgay@redhat.com>
-#
+#           Sayan Chowdhury <sayanchowdhury@fedoraproject.org>
 
-import argparse
-import ConfigParser
-
+import toml
 
 # Read in config file
-config = ConfigParser.RawConfigParser()
-config.read('/etc/fedimg.cfg')
+with open("/etc/fedimg/fedimg-conf.toml") as conffile:
+    config = toml.loads(conffile.read())
 
 # Fedimg Consumer configurations
-PROCESS_COUNT =
-STATUS_FILTER = ('FINISHED_INCOMPLETE', 'FINISHED',)
-
-
-
+PROCESS_COUNT = 4
+STATUS_FILTER = ('FINISHED_INCOMPLETE', 'FINISHED')
 
 CLEAN_UP_ON_FAILURE = config.get('general', 'clean_up_on_failure')
 DELETE_IMAGES_ON_FAILURE = config.get('general', 'delete_images_on_failure')
-
-# koji_server is the location of the Koji hub that should be used
-# to initialize the Koji connection.
-KOJI_SERVER = config.get('koji', 'server')
-
-# The two slashes ("//") in the following URL are NOT a mistake.
-BASE_KOJI_TASK_URL = config.get('koji', 'base_task_url')
 
 # AMAZON WEB SERVICES (EC2)
 AWS_UTIL_USER = config.get('aws', 'util_username')
 AWS_TEST_USER = config.get('aws', 'test_username')
 AWS_ACCESS_ID = config.get('aws', 'access_id')
 AWS_SECRET_KEY = config.get('aws', 'secret_key')
-AWS_KEYNAME = config.get('aws', 'keyname')
-AWS_KEYPATH = config.get('aws', 'keypath')
-AWS_PUBKEYPATH = config.get('aws', 'pubkeypath')
-AWS_UTIL_VOL_SIZE = config.get('aws', 'util_volume_size')
-AWS_TEST_VOL_SIZE = config.get('aws', 'test_volume_size')
-AWS_TEST = config.get('aws', 'test')
-AWS_AMIS = config.get('aws', 'amis')
+AWS_VOL_SIZE = config.get('aws', 'volume_size')
 AWS_IAM_PROFILE = config.get('aws', 'iam_profile')
 
 # RACKSPACE
