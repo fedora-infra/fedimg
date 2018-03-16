7bdd06f568856c010a4eacb1e70c262fa4c3388c
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2018-03-16 21:34:16 +0530

    scripts: Fix the manual upload trigger script
    Signed-off-by: Sayan Chowdhury <sayan.chowdhury2012@gmail.com>

--- bin/trigger_upload.py
+++ bin/trigger_upload.py
@@ -1,8 +1,6 @@
 #!/bin/env python
 # -*- coding: utf8 -*-
-
-""" Triggers an upload process with the specified raw.xz URL. Useful for
-    manually triggering Fedimg jobs. """
+""" Triggers an upload process with the specified raw.xz URL. """
 
 import logging
 import logging.config
@@ -18,8 +16,8 @@ from fedimg.services.ec2 import EC2Service, EC2ServiceException
 import fedimg.uploader
 from fedimg.util import virt_types_from_url
 
-if len(sys.argv) != 2:
-    print 'Usage: trigger_upload.py <rawxz_image_url>'
+if len(sys.argv) != 3:
+    print 'Usage: trigger_upload.py <rawxz_image_url> <compose_id>'
     sys.exit(1)
 
 logging.config.dictConfig(fedmsg.config.load_config()['logging'])
@@ -28,5 +26,11 @@ log = logging.getLogger('fedmsg')
 upload_pool = multiprocessing.pool.ThreadPool(processes=4)
 
 url = sys.argv[1]
+compose_id = sys.argv[2]
 
-fedimg.uploader.upload(upload_pool, [url])
+compose_meta = {
+    'compose_id': compose_id
+}
+
+fedimg.uploader.upload(upload_pool, [url], compose_meta=compose_meta)
+
