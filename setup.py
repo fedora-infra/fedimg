b206d76daf70ace3d83b6f9b5e3fbe77b3e1184a
Euan Kemp <euank@redhat.com>
2018-04-03 00:13:18 -0700

    setup.py: add 'fedfind' dependency

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=['fedmsg',
+                      'fedfind',
                       'apache-libcloud',
                       'paramiko',
                       'toml'],
